c624cd227c6477706bec8b66f2967f9c9f8607e9
limouren_soul <2123335217@qq.com>
2019-03-28 15:36:48 +0800

    test conflict left

--- src/main/java/cn/lsk/test1/Happy.java
+++ src/main/java/cn/lsk/test1/Happy.java
@@ -1,9 +1,9 @@
 package cn.lsk.test1;
 
 public class Happy {
-	private void hell0() {
+	public void hell0() {
 		// TODO 自动生成的方法存根
-		System.out.println("nihao...right");
+		System.out.println("nihao.....left");
 	}
 
 }
